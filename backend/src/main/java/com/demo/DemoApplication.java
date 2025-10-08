c74c312fae91dc88956068e6b5353ba288728851
Rajat Jana <2742425@tcsgenailab.onmicrosoft.com>
2025-10-08 08:19:55 +0000

    Updated feature X / fixed bug Y

--- backend/src/main/java/com/demo/DemoApplication.java
+++ backend/src/main/java/com/demo/DemoApplication.java
@@ -2,10 +2,13 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; // 👈 Import this
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer { // 👈 Add this part
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
 }
